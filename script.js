6c37ccd3c4aed11bfba0fe11c7328da662eefd5a
Arthur Barata <99782169+Arthur-Barata@users.noreply.github.com>
2022-06-11 18:37:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$("#te").click(function (){
+    $("#te").css("background-image", "none")
+    $("#tex").text("Te")
+})
+$("#amo").click(function (){
+    $("#amo").css("background-image", "none")
+    $("#tex2").text("Amo")
+})
+
